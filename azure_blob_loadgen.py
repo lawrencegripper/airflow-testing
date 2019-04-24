e059cd2491a1507c5b759861b6fa7d540be78d19
Lawrence Gripper <info@grippers.co.uk>
2019-04-24 12:06:43 +0100

    Tidy up load gen

--- azure_blob_loadgen.py
+++ azure_blob_loadgen.py
@@ -2,12 +2,7 @@ from datetime import timedelta, datetime
 import airflow
 from airflow import DAG
 from airflow.operators.dagrun_operator import TriggerDagRunOperator
-from airflow.contrib.sensors.wasb_sensor import WasbPrefixSensor
-from airflow.contrib.hooks.wasb_hook import WasbHook
-from azure.storage.blob.models import BlobPermissions
 from airflow.operators.python_operator import PythonOperator
-from airflow.operators.dummy_operator import DummyOperator
-from airflow.operators.subdag_operator import SubDagOperator
 
 args = {
     'owner': 'airflow',
@@ -23,6 +18,7 @@ dag = DAG(
 image_url = "https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.gpwebsolutions-host.co.uk%2F1400%2Ffiles%2F2014%2F02%2Fprescription1.jpg&f=1"
 
 def start_image_processing(**context):
+    print("Start load gen")
     for x in range (0, 100):
         def trigger_processing_dag(context, dag_run_obj):
                 dag_run_obj.payload = {
@@ -36,6 +32,8 @@ def start_image_processing(**context):
             python_callable=trigger_processing_dag,
             dag=dag
         ).execute(context)
+    print("Finish load gen")
+    
 
 python_task = PythonOperator(
     task_id='start',
